92bec57cd4bc8fa8994d19e57b54fcca652fcbc8
Mohamed Dahani <mohamedxdahani@gmail.com>
2025-08-26 20:43:43 +0100

    try to understand the code

--- level_01/ft_picoshell/picoshell.c
+++ level_01/ft_picoshell/picoshell.c
@@ -3,30 +3,48 @@
 
 int picoshell(char **cmds[])
 {
+    int fds[2];
+    int prev_fd = -1;
+
     if (!cmds || !cmds[0])
         return (1);
-    int pipefds[2];
-    int prev_fd = -1;
     int i = 0;
     while (cmds[i])
     {
-            int has_next = (cmds[i + 1]);
-            if (has_next && pipe(pipefds) == -1)
-                return (1);
+        int has_next = (cmds[i + 1] != NULL);
+        if (has_next && pipe(fds) == -1)
+            return (1);
         int pid = fork();
-        if (pid == -1)
+        if (pid == -1 && has_next)
         {
-            if (has_next)
-            {
-                close(pipefds[0]);
-                close(pipefds[1]);
-            }
-            return (1);            
+            close(fds[0]);
+            close(fds[1]);
+            return (1);
         }
         if (pid == 0)
         {
+            if (prev_fd != -1)
+            {
+                close(fds[1]);
+                dup2(prev_fd, STDIN_FILENO);
+            }
+            if (has_next)
+            {
+                close(fds[0]);
+                dup2(fds[1], STDOUT_FILENO);
+                close(fds[1]);
+            }
             execvp(cmds[i][0], cmds[i]);
-            exit(1);
+        }
+        else
+        {
+            if (prev_fd != -1)
+                close(prev_fd);        
+            if (has_next)
+            {
+                close(fds[1]);
+                prev_fd = fds[0];
+            }
         }
         i++;
     }
@@ -40,8 +58,8 @@ int picoshell(char **cmds[])
 int main(void)
 {
     char *ls[] = {"ls", NULL};
-    // char *wc[] = {"wc", "-l", NULL};
-    char **cmds[] = {ls, NULL};
+    char *wc[] = {"wc", "-l", NULL};
+    char **cmds[] = {ls, wc, NULL};
 
     return picoshell(cmds);
 }
