6c0f1c145eae4763185f2a67c4e1c997ffccec54
Mohamed Dahani <mohamedxdahani@gmail.com>
2025-08-26 12:18:52 +0100

    finish the ft_popen

--- level_01/ft_popen/ft_popen.c
+++ level_01/ft_popen/ft_popen.c
@@ -3,52 +3,52 @@
 
 int ft_popen(const char *file, char *const argv[], char type)
 {
-    int pipefds[2];
+    int fds[2];
     
-    if (!file || !argv || (type != 'r' && type != 'r'))
+    if (!file || !argv || (type != 'r' && type != 'w'))
+        return (-1);    
+    if (pipe(fds) == -1)
         return (-1);
-    if (pipe(pipefds) == -1)
+    int pid = fork();
+    if (pid == -1)
         return (-1);
-    if (fork() == 0)
+    if (pid == 0)
     {
         if (type == 'r')
         {
-            close(pipefds[0]);
-            if (dup2(pipefds[1], 1) == -1)
-            {
-                close(pipefds[1]);
-                exit(-1);   
-            }
-            close(pipefds[1]);
+            close(fds[0]);
+            if (dup2(fds[1], STDOUT_FILENO) == -1)
+                exit(-1);
+            close(fds[1]);
         }
         else
         {
-            close(pipefds[1]);
-            if (dup2(pipefds[0], 0) == -1)
-            {
-                close(pipefds[0]);
+            close(fds[1]);
+            if (dup2(fds[0], STDIN_FILENO) == -1)
                 exit(-1);
-            }
-            close(pipefds[0]);
+            close(fds[0]);
         }
         execvp(file, argv);
-        exit(-1);
-    }
-    if (type == 'r')
-    {
-        close(pipefds[1]);
-        return (pipefds[0]);
     }
     else
     {
-        close(pipefds[0]);
-        return (pipefds[1]);
+        if (type == 'r')
+        {
+            close(fds[1]);
+            return(fds[0]);
+        }
+        else
+        {
+            close(fds[0]);
+            return(fds[1]);
+        }
     }
-    
+    return(0);
 }
 
 #include <stdio.h>
 
+// read
 int main(void)
 {
     int fd;
@@ -64,5 +64,16 @@ int main(void)
     close(fd);
     return 0;
 }
+
+// write
+// #include <string.h>
+// int main(void)
+// {
+//     int fd = ft_popen("cat", (char *const []){"cat", NULL}, 'w');
+//     char *msg = "Hello from ft_popen with type 'w'!\n";
+//     write(fd, msg, strlen(msg));
+//     close(fd);
+//     return 0;
+// }
 
 
