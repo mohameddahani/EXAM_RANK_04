93a44d9548e54a2dd7fa0ef69a4fa9f4cafee800
Mohamed Dahani <mohamedxdahani@gmail.com>
2025-08-27 19:19:37 +0100

    finish the picoshell

--- level_01/ft_picoshell/picoshell.c
+++ level_01/ft_picoshell/picoshell.c
@@ -1,65 +1,59 @@
 #include <unistd.h>
 #include <stdlib.h>
+#include <sys/wait.h>
 
 int picoshell(char **cmds[])
 {
-    int fds[2];
-    int prev_fd = -1;
+	int fds[2], prev_fd = -1;
 
-    if (!cmds || !cmds[0])
-        return (1);
-    int i = 0;
-    while (cmds[i])
-    {
-        int has_next = (cmds[i + 1] != NULL);
-        if (has_next && pipe(fds) == -1)
-            return (1);
-        int pid = fork();
-        if (pid == -1 && has_next)
-        {
-            close(fds[0]);
-            close(fds[1]);
-            return (1);
-        }
-        if (pid == 0)
-        {
-            if (prev_fd != -1)
-            {
-                close(fds[1]);
-                dup2(prev_fd, STDIN_FILENO);
-            }
-            if (has_next)
-            {
-                close(fds[0]);
-                dup2(fds[1], STDOUT_FILENO);
-                close(fds[1]);
-            }
-            execvp(cmds[i][0], cmds[i]);
-        }
-        else
-        {
-            if (prev_fd != -1)
-                close(prev_fd);        
-            if (has_next)
-            {
-                close(fds[1]);
-                prev_fd = fds[0];
-            }
-        }
-        i++;
-    }
-    return (0);
+	if (!cmds || !cmds[0])
+		return (1);
+	int i = 0;
+	while (cmds[i])
+	{
+		int has_next = (cmds[i + 1] != NULL);
+		if (has_next && pipe(fds) == -1)
+			return (1);
+		int pid = fork();
+		if (pid == -1)
+			return (1);
+		if (pid == 0)
+		{
+			if (prev_fd != -1)
+			{
+				dup2(prev_fd, STDIN_FILENO);
+				close(prev_fd);
+			}
+			if (has_next)
+			{
+				close(fds[0]);
+				dup2(fds[1], STDOUT_FILENO);
+				close(fds[1]);
+			}
+			execvp(cmds[i][0], cmds[i]);
+			exit(1);
+		}
+		else
+		{
+			if (prev_fd != -1)
+				close(prev_fd);	
+			if (has_next)
+			{
+				close(fds[1]);
+				prev_fd = fds[0];
+			}
+		}
+		i++;
+	}
+	while (wait(NULL) > 0)
+		;
+	return (0);
 }
 
-
-
-
-
-int main(void)
+int main()
 {
-    char *ls[] = {"ls", NULL};
-    char *wc[] = {"wc", "-l", NULL};
-    char **cmds[] = {ls, wc, NULL};
-
-    return picoshell(cmds);
+	char *ls[] = {"ls", NULL};
+	char *wc[] = {"wc", "-l", NULL};
+	char **cmds[] = {ls, wc, NULL};
+	return (picoshell(cmds));
 }
