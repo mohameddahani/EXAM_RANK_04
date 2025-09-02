1d402c90f2476b477a743a52b2ee878186e4ed87
Mohamed Dahani <mohamedxdahani@gmail.com>
2025-09-02 14:33:30 +0100

    fix some bugs

--- level_02/ft_vbc/no_tree.c
+++ level_02/ft_vbc/no_tree.c
@@ -1,107 +1,103 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   no_tree.c                                          :+:      :+:    :+:   */
+/*   vbc.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mdahani <mdahani@student.42.fr>            +#+  +:+       +#+        */
+/*   By: mohameddahani <mohameddahani@student.42.fr> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/08/31 16:50:11 by mdahani           #+#    #+#             */
-/*   Updated: 2025/08/31 19:21:26 by mdahani          ###   ########.fr       */
+/*   Created: 2025/09/01 19:54:04 by mohameddahani     #+#    #+#             */
+/*   Updated: 2025/09/01 19:54:04 by mohameddahani    ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <ctype.h>
 
-int	unexpected(char c)
+int unexpected(char c)
 {
-	if (!c)
-		printf("Unexpected end of input\n");
-	else
-		printf("Unexpected token '%c'\n", c);
-	return (-1);
+    if (c == '\0')
+        printf("Unexpected end of input\n");
+    else
+        printf("Unexpected token '%c'\n", c);
+    return -1;
 }
 
-int	eval(const char *s, int *i)
+int eval(const char *s, int *i)
 {
-	int		result;
-	int		term;
-	int		factor;
-
-	result = 0;
-	
-	while (s[*i] && s[*i] != ')')
-	{
-		term = 1;
-		
-		// Parse term (handle multiplication)
-		while (1)
-		{
-			// Parse factor
-			if (s[*i] == '(')
-			{
-				(*i)++;
-				factor = eval(s, i);
-				if (factor == -1)
-					return (-1);
-				if (s[*i] != ')')
-					return (unexpected(s[*i]));
-				(*i)++;
-			}
-			else if (isdigit(s[*i]))
-			{
-				factor = s[*i] - '0';
-				(*i)++;
-			}
-			else
-				return (unexpected(s[*i]));
-			
-			term *= factor;
-			
-			// Check if next operator is multiplication
-			if (s[*i] == '*')
-			{
-				(*i)++;
-				if (!s[*i])
-					return (unexpected(s[*i]));
-			}
-			else
-				break;
-		}
-		
-		// Apply the previous operator to result
-		result += term;
-		
-		// Check for next operator
-		if (s[*i] == '+')
-		{
-			(*i)++;
-			if (!s[*i])
-				return (unexpected(s[*i]));
-		}
-		else
-			break;
-	}
+    int result = 0;
+    int term = 1;
+    int factor;
     
-	return (result);
+    while (s[*i] && s[*i] != ')')
+    {
+        // Parse factor
+        if (s[*i] == '(')
+        {
+            (*i)++;
+            factor = eval(s, i);
+            if (factor == -1)
+                return -1;
+            if (s[*i] != ')')
+                return unexpected(s[*i]);
+            (*i)++;
+        }
+        else if (isdigit(s[*i]))
+        {
+            factor = s[*i] - '0';
+            (*i)++;
+        }
+        else
+            return unexpected(s[*i]);
+        
+        // Apply multiplication
+        term *= factor;
+        
+        // Check next character
+        if (s[*i] == '*')
+        {
+            (*i)++;
+            if (!s[*i] || s[*i] == ')' || s[*i] == '+' || s[*i] == '*')
+                return unexpected(s[*i]);
+        }
+        else
+        {
+            // End of multiplication chain, add to result
+            result += term;
+            term = 1;
+            
+            if (s[*i] == '+')
+            {
+                (*i)++;
+                if (!s[*i] || s[*i] == ')' || s[*i] == '+' || s[*i] == '*')
+                    return unexpected(s[*i]);
+            }
+            else
+                break;
+        }
+    }
+    
+    return result;
 }
 
-int	main(int ac, char **av)
+int main(int argc, char **argv)
 {
-	int	res;
-	int	i;
+    int i = 0;
+    int result;
 
-	if (ac != 2)
-		return (1);
-	i = 0;
-	res = eval(av[1], &i);
-	if (res == -1)
-		return (1);
-	if (av[1][i])
-	{
-		unexpected(av[1][i]);
-		return (1);
-	}
-	printf("%d\n", res);
-	return (0);
+    if (argc != 2)
+        return 1;
+
+    if (!argv[1][0] || (!isdigit(argv[1][0]) && argv[1][0] != '('))
+        return (unexpected(argv[1][0]), 1);
+
+    result = eval(argv[1], &i);
+    
+    if (result == -1)
+        return 1;
+        
+    if (argv[1][i])
+        return (unexpected(argv[1][i]), 1);
+
+    printf("%d\n", result);
+    return 0;
 }
